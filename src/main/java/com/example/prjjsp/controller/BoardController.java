2b84f4771863d3b6879c93ebec12c186d9b19d4a
Lemonstew <colt4949@naver.com>
2024-10-22 10:30:04 +0900

    게시물 저장 후 redirect

--- src/main/java/com/example/prjjsp/controller/BoardController.java
+++ src/main/java/com/example/prjjsp/controller/BoardController.java
@@ -1,14 +1,20 @@
 package com.example.prjjsp.controller;
 
+import com.example.prjjsp.dto.Board;
+import com.example.prjjsp.service.BoardService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("/board")
 public class BoardController {
+
+    private final BoardService service;
+
     // 게시물 CRUD
 
     //  /board/new
@@ -19,4 +25,14 @@ public class BoardController {
 
 
     }
+
+    @PostMapping("new")
+    public String newBoard(Board board) {
+
+        System.out.println(board);
+        service.add(board);
+
+        return "redirect:/board/new";
+    }
 }
+
